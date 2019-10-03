4295143e02400e1cdb2d0642953dc60268755210
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-03 14:10:41 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ import os
 
 def wsgi_app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text-plain')])    
-    return  "\n".join(os.environ.get('QUERY_STRING')).split("&")
+    return  os.environ.get('QUERY_STRING')
 
 # test
