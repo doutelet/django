e8790db0168eae9c583accef33537b86cec63eae
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-03 14:22:16 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ from os import environ
 
 def wsgi_app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text-plain')])    
-    return [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    return [(i + '\n') for i in environ.get('QUERY_STRING').split('&')]
 
 # test
