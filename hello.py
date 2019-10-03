4647ccbeba4135d962d57c71fd8ab03fcafab145
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-03 17:09:25 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ from os import environ
 
 
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text-plain')])    
+    start_response('200 OK', [('Content-Type', 'text-plain')])
+    print(environ['QUERY_STRING'])
     return [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
 
 # test
