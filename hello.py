953e21cb273aec23e00f61006d3a7f2f78b76567
doutelet <doutelet@gmail.com>
2019-10-03 10:57:08 +0000

    rework

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ def wsgi_app(env, start_response):
     start_response('200 OK', [('Content-Type', 'text-plain')])
     temp = []
     for el in environ.get['QUERY_STRING']:
-    temp.append(el)
+        temp.append(el)
     return temp
 
 # test
