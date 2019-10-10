16b036cbc4db9be3313963670cb205d2f75e0e77
doutelet <55977726+doutelet@users.noreply.github.com>
2019-10-10 13:02:28 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from ask.qa import views
+from qa import views
 
 
 
